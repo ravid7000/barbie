cdb8e7f86e155567b6da860dbbb51c79455ebdbf
Ravi Dhiman <ravid7000@gmail.com>
2023-08-07 00:09:55 +0530

    tailwind setup

---
+++ src/lib/ui/Layout/Layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import '../init.css'
+</script>
+
+<header>Header</header>
+
+<main>
+  <slot />
+</main>
+
+<footer>Footer</footer>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  // UI
+  import Layout from '$lib/ui/Layout/Layout.svelte'
+  ///- UI
+</script>
+
+<svelte:head>
+  <title>Variables</title>
+</svelte:head>
+
+<Layout>
+  <slot />
+</Layout>
